336391d29dcf233c64fb475d5fef8aa3ba646eaf
Audric <audric.laroche@gmail.com>
2018-11-28 16:35:38 +0100

    fixed the navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_110326) do
+ActiveRecord::Schema.define(version: 2018_11_27_161255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "progresses", force: :cascade do |t|
-    t.integer "scale"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_progresses_on_user_id"
-  end
 
   create_table "results", force: :cascade do |t|
     t.text "url"
@@ -57,7 +49,6 @@ ActiveRecord::Schema.define(version: 2018_11_28_110326) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "progresses", "users"
   add_foreign_key "results", "searches"
   add_foreign_key "searches", "users"
 end
