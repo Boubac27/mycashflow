f11cefea863f147c325ceb34f7eacb7f5c996fde
Etienne Lioret <kavatine99@gmail.com>
2018-11-29 11:14:30 -0500

    Corrige migration pour production

--- db/migrate/20181129161346_create_progresses.rb
+++ db/migrate/20181129161346_create_progresses.rb
@@ -2,6 +2,7 @@ class CreateProgresses < ActiveRecord::Migration[5.2]
   def change
     create_table :progresses do |t|
       t.integer :scale
+      t.references :user, foreign_key: true
     end
   end
 end
