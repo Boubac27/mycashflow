3d685259e7963d02a59bcebe97c6de3f69d721c8
Etienne Lioret <kavatine99@gmail.com>
2018-11-29 07:07:52 -0500

    Bien avancé sur l'ajout de la barre de progression

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,6 +1,8 @@
 class ProgressesController < ApplicationController
   def index
     @progress = Progress.where("user_id=?", current_user.id).first
-    @scale = @progress.scale
+    @scale = {}
+    @scale[:scale] = @progress.scale if !@progress.nil?
+    render json: @scale
   end
 end
--- app/javascript/packs/progr.js
+++ app/javascript/packs/progr.js
@@ -1,11 +1,32 @@
-function updateProgressBar() {
-  page_new = document.getElementById('results');
+console.log("dans progr");
+
+const updateProgressBar = () => {
+  const page_new = document.getElementById('results');
+  console.log(page_new);
   if (page_new) {
-    window.setInterval(update, 200)
+    window.setInterval(update, 200);
   }
-}
+};
 
 function update() {
-  const url = `localhost:3000/users/${user_id}/progresses`
-  fetch(url).then((response) => { return response.json();} ).then((data) => console.log(data));
+  const user_id = document.querySelector('h6').innerText;
+  const url = `http://localhost:3000/users/${user_id}/progresses`;
+  fetch(url).then((response) => { return response.json();} ).then((data) => {
+    const squares = document.querySelectorAll('.fas .fa-square-full');
+    console.log(squares);
+    let counter = 0;
+    squares.forEach((square) => {
+      if (counter < data) {
+        console.log("counter < data");
+        square.style.color = "black"
+      }
+      else{
+        console.log("dans le else");
+        square.style.color = "white"
+      }
+      counter += 1;
+    });
+  });
 }
+
+export { updateProgressBar, update }
