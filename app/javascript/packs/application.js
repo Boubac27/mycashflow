048a5a670ad2e4034e1747f05dcd3fd47a2c6ff1
Audric <audric.laroche@gmail.com>
2018-11-30 15:43:35 +0100

    animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
+import { displayAnimation } from './animation';
 import "bootstrap";
 import mapboxgl from 'mapbox-gl';
 import { displayMap } from './map';
 import { updateProgressBar } from './progr';
+
 displayMap();
+displayAnimation();
 updateProgressBar();
 window.markers = [];
-
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,14 +1,19 @@
 import mapboxgl from 'mapbox-gl';
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcG1pIiwiYSI6ImNqcDEwbjc1bTJqZXkzcXMzdmp6eXgyYjEifQ.04zRl-cA0VHPErxPv-Q9pw';
 window.mapboxgl = mapboxgl;
 
 const displayMap = () => {
-  window.map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [ 4.84977, 45.76931 ],
-    zoom: 12
-  });
+  const map = document.querySelector('#map');
+
+  if (map) {
+    window.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [ 4.84977, 45.76931 ],
+      zoom: 12
+    });
+  }
 }
 
 export { displayMap };
