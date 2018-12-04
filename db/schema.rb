047b486c4ab7e52480e559dad5e71abdb220bca2
Stephane Milioti <s.milioti@orange.fr>
2018-12-04 09:37:24 +0100

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_12_03_135334) do
-
 ActiveRecord::Schema.define(version: 2018_12_03_155209) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,14 +39,14 @@ ActiveRecord::Schema.define(version: 2018_12_03_155209) do
     t.datetime "updated_at", null: false
     t.float "lat"
     t.float "long"
+    t.string "city"
+    t.string "zipcode"
     t.float "property_taxe"
     t.float "work"
     t.float "insurance"
     t.float "total_rent"
     t.float "monthy_loan"
     t.float "rental_charges"
-    t.string "city"
-    t.string "zipcode"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
