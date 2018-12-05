5f93d371e9b61d04dcb2d6f7f0d80c77295e534f
Stephane Milioti <s.milioti@orange.fr>
2018-12-05 10:58:27 +0100

    reorganize js

--- app/javascript/autocomplete.js
+++ app/javascript/autocomplete.js
@@ -1,40 +1,114 @@
-// TODO: Autocomplete the input with AJAX calls.
-const input = document.querySelector('#search');
-const results = document.querySelector('#results');
+const initAutocomplete = () => {
 
-const drawResponseList = (data) => {
-  results.innerHTML = '';
-  data.words.forEach((word) => {
-    results.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
-  });
-};
+    console.log("je suis la");
+    console.log(document.getElementById('map2'));
 
-const autocomplete = (e) => {
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+#{word}&key=AIzaSyAZuR-94uFzg2VuikctpXZHBOcFcuX0Wzo`,{
+    if (document.getElementById('map2') == null) { return }
 
-  })
-    .then(response => response.json())
-    .then((data) => { drawResponseList(data);
-                        console.log(data["results"][0]["address_components"]);
+    var map = new google.maps.Map(document.getElementById('map2'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap'
     });
-};
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-input.addEventListener('keyup', autocomplete);
-console.log("hello!");
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 
+    var markers = [];
 
-// const input = document.querySelector('#search');
-// const results = document.querySelector('#results');
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+      if (places.length == 0) {
+        return;
+      }
+      else {
+        places[0]["address_components"].forEach((component) => {
+          console.log(component["types"][0]);
+          if (component["types"][0] === 'locality') {
+            textarea_city = document.getElementById('city');
+            textarea_city.innerText = component["long_name"];
+          }
+          if (component["types"][0] === 'postal_code') {
+            textarea_zipcode = document.getElementById('zipcode');
+            textarea_zipcode.innerText = component["long_name"];
+          }
+        });
+      }
 
-// const autocomplete = (e) => {
-//   fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+lyon&key=AIzaSyAZuR-94uFzg2VuikctpXZHBOcFcuX0Wzo`)
-// }
-//     .then(response => response.json())
-//     .then((data) => {
-//       results.innerHTML = '';
-//       data.words.forEach(function(word) => {
-//         results.insertAdjacentHTML('beforeend', `<li>${word}</li>`);
-//   });
-// };
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
 
-// input.addEventListener('keyup', autocomplete);
+      var bounds = new google.maps.LatLngBounds();
+
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+    document.getElementById('button').addEventListener('click', function(evt) {
+      evt.preventDefault();
+      console.log("bouton clique");
+      const loader = document.getElementById('loader');
+      loader.classList.remove('loader')
+
+      const textarea_city = document.getElementById('city').value;
+      const textarea_zipcode = document.getElementById('zipcode').value;
+      const budget = document.getElementById('budget').value;
+      const token = document.getElementById('tok').value;
+      let mhash = {};
+      console.log(textarea_city);
+      console.log(textarea_zipcode);
+      console.log(budget);
+      mhash["city"] = textarea_city;
+      mhash["zipcode"] = textarea_zipcode;
+      mhash["budget"] = budget;
+      mhash["authenticity_token"] = token;
+      console.log(mhash);
+
+      fetch("/searches", { method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(mhash)
+      }).then((response) => {
+        return response.text();
+      }).then((data) => {
+        //console.log(data);
+        loader.classList.add('loader')
+        eval(data);
+      });
+    });
+  }
+
+export { initAutocomplete };
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,14 @@
 import "bootstrap";
 import mapboxgl from 'mapbox-gl';
-import { displayMap } from './map';
+import { initMap } from './map';
 import { updateProgressBar } from './progr';
+import { initAutocomplete } from "../autocomplete.js"
+
+window.initGoogleJs = function() {
+  console.log(initAutocomplete);
+  initAutocomplete();
+  initMap();
+}
 
 updateProgressBar();
 window.markers = [];
